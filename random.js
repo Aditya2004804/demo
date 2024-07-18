025e76527e24f1e3ea87a17b64450ff65de598f4
Aditya kumar <123500836+Aditya2004804@users.noreply.github.com>
2024-07-18 22:38:10 +0530

    Add files via upload

---
+++ random.js
@@ -0,0 +1,80 @@
+// let generateImageForm = 
+// 	document.getElementById('generate-image-form'); 
+// let formInput = 
+// 	document.getElementById('input-value'); 
+// let imageContainerText = 
+// 	document.getElementById('imageContainerText'); 
+// let imageGenerated = 
+// 	document.getElementById('generated-image'); 
+// let imageContainer = 
+// 	document.getElementById('images-visible'); 
+
+// async function fetchImages(category) { 
+// 	try { 
+// 		let response = 
+// 		await fetch(`use a API`); 
+// 		if (!response.ok) { 
+// 			throw new Error('Unable to fetch the data'); 
+// 		} 
+// 		imageContainerText.innerText = 
+// 		"Below is your generated Image:"; 
+// 		imageContainer.style.display = "block"; 
+// 		imageGenerated.src = response.url; 
+// 		console.log(response.url); 
+// 	} 
+// 	catch (error) { 
+// 		console.log(error); 
+// 	} 
+// } 
+
+// generateImageForm.addEventListener('submit', (e) => { 
+// 	e.preventDefault(); 
+// 	let enteredText = formInput.value; 
+// 	if (enteredText !== "") { 
+// 		fetchImages(enteredText); 
+// 	} 
+// 	else { 
+// 		imageContainerText.innerText = 
+// 			"Input field can not be empty!"; 
+// 	} 
+// })
+
+const API_KEY = '[YOUR API KEY HERE]';
+const submiticon = document.querySelector('#submit-icon');
+const inputElement= document.querySelector('input');
+const imageSection = document.querySelector('.image-section');
+
+const getImages = async () => { 
+    const options = {
+        method: 'POST',
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            'prompt': inputElement.value,
+            'n':2,
+            'size':"1024x1024"
+        })
+    }
+    try 
+    {
+       const response = await fetch('https://api.openai.com/v1/images/generations', options)
+         const data = await response.json()
+       
+            data?.data.forEach((imageObject) => 
+            {
+                const ImageContainer = document.createElement('div')
+                ImageContainer.classList.add('image-container')
+                const imageElement = document.createElement('img')
+                imageElement.setAttribute('src', imageObject.url)
+                ImageContainer.append(imageElement)
+                imageSection.append(ImageContainer)
+            })
+    }
+    catch (error)
+    {
+        console.log(error)
+    }
+}
+submiticon.addEventListener('click', getImages)
